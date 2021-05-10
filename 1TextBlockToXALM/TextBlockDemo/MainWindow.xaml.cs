d7c476ab1bcb149dd05098e8346dd4c5b96fc4ab
Juan@SDSharp <sebasortizsanta@gmail.com>
2021-05-09 19:47:02 -0500

    Textcode in Code Behind

--- 1TextBlockToXALM/TextBlockDemo/MainWindow.xaml.cs
+++ 1TextBlockToXALM/TextBlockDemo/MainWindow.xaml.cs
@@ -23,6 +23,27 @@ namespace TextBlockDemo
         public MainWindow()
         {
             InitializeComponent();
+
+            myTextBlock.Text = "Hello from the cs side";
+            myTextBlock.Foreground = Brushes.Blue;
+
+            CustomTextCreation();
+        }
+
+        private void CustomTextCreation()
+        {
+            TextBlock myTb = new TextBlock();
+            myTb.Text = "Hello World";
+            myTb.Inlines.Add("This is added using inlines");
+            myTb.Inlines.Add(new Run(" Run text that I added in Code behind")
+            {
+                Foreground = Brushes.Red,
+                TextDecorations = TextDecorations.Underline,
+                FontSize = 15
+            });
+            myTb.TextWrapping = TextWrapping.Wrap;
+            myTb.Foreground = Brushes.DarkCyan;
+            this.Content = myTb;
         }
 
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
